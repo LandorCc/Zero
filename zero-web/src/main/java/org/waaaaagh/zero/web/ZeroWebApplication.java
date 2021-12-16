7cc0400d2c6b41f9c00425896f8eedd3c8641cb0
waaaaagh <shaojiedoc@163.com>
2021-12-16 11:19:06 +0800

    修改配置文件,注册nacos

--- zero-web/src/main/java/org/waaaaagh/zero/web/ZeroWebApplication.java
+++ zero-web/src/main/java/org/waaaaagh/zero/web/ZeroWebApplication.java
@@ -4,11 +4,13 @@ package org.waaaaagh.zero.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author waaaaagh
  * @since 2021/12/15 15:03
  */
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ZeroWebApplication {
 
